b569bb201f5e1a751cde42a20bb8849dd1b45143
The_Trolling_stone <adpiero1897@mail.ru>
2022-04-27 19:48:31 +0300

    Empty commit

--- src/main/java/ru/gb/gbrestmart/GbRestMartApplication.java
+++ src/main/java/ru/gb/gbrestmart/GbRestMartApplication.java
@@ -10,6 +10,10 @@ import ru.gb.gbrestmart.entity.Product;
 import ru.gb.gbrestmart.service.ProductService;
 
 import java.util.HashSet;
+/*1. Создать микросервис для работы с заказами. Данный микросервис должен уметь создавать заказ, удалять заказ, добавлять
+        в заказ товары, удалять товары из заказа
+        2. Проект приложенный в архиве запустить и коммитить по схеме описанной в схеме по работе с гитом в 8 уроке
+        3. Доработать код из архива gb-rest-mart так, что пользователю было предоставлено API по работе с заказом*/
 
 @SpringBootApplication
 public class GbRestMartApplication {
